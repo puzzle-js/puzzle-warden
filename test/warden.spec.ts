6ed67388580e703dc4947f4073fab3b52034d293
Ahmet Can <ahmetcanguven44@gmail.com>
2019-01-17 23:19:08 +0300

    Fix tests

--- test/warden.spec.ts
+++ test/warden.spec.ts
@@ -1,12 +1,27 @@
 import "reflect-metadata";
 
+import sinon, {SinonMock} from "sinon";
 import {Warden} from "../src/warden";
 import {expect} from "chai";
+import {Cache} from "../src/cache/cache";
+
+const sandbox = sinon.createSandbox();
+const cache = new Cache();
+
+let cacheMock: SinonMock;
 
 describe("[sentry.ts]", () => {
+  afterEach(() => {
+    sandbox.verifyAndRestore();
+  });
+
+  beforeEach(() => {
+    cacheMock = sandbox.mock(cache);
+  });
+
   it("should create new Sentry instance", () => {
     // Arrange
-    const sentry = new Warden();
+    const sentry = new Warden(cache);
 
     // Assert
     expect(sentry).to.be.instanceOf(Warden);
