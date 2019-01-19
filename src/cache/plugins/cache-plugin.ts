c2cafd9187694294fb1062b700fda8e452f70ba6
Emre-Kul <emre.kul@trendyol.com>
2019-01-19 06:12:10 +0300

    couchbase cache plugin added

--- src/cache/plugins/cache-plugin.ts
+++ src/cache/plugins/cache-plugin.ts
@@ -1,4 +1,11 @@
 export abstract class CachePlugin {
-  abstract get(key: string): null | string;
-  abstract set(set: string): void;
+  abstract get(key: string): string | null | Promise<string>;
+  abstract set(key: string, value: any): Promise<void>;
+}
+
+export interface CouchbaseOptions {
+  cluster: string;
+  username: string;
+  password: string;
+  bucket: string;
 }
