62923fe20e3f4f08ff0cebc4fe9e11c5eac17587
Ahmet Can Güven <ahmetcan.guven@trendyol.com>
2019-03-26 11:20:10 +0300

    Project expose

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-// import {CacheFactory} from "./cache-factory";
-// import {Tokenizer} from "./tokenizer";
-// import {RequestWrapper} from "./request-wrapper";
-// import {StreamFactory} from "./stream-factory";
-// import {RequestManager} from "./request-manager";
-// import {Warden} from "./warden";
-//
-// const cacheFactory = new CacheFactory();
-// const tokenizer = new Tokenizer();
-// const requestWrapper = new RequestWrapper();
-// const streamFactory = new StreamFactory(cacheFactory, requestWrapper);
-// const requestManager = new RequestManager(streamFactory, tokenizer);
-// const warden = new Warden(requestManager, requestWrapper);
-//
-// module.exports = warden;
+import {CacheFactory} from "./cache-factory";
+import {Tokenizer} from "./tokenizer";
+import {RequestWrapper} from "./request-wrapper";
+import {StreamFactory} from "./stream-factory";
+import {RequestManager} from "./request-manager";
+import {Warden} from "./warden";
+
+const cacheFactory = new CacheFactory();
+const tokenizer = new Tokenizer();
+const requestWrapper = new RequestWrapper();
+const streamFactory = new StreamFactory(cacheFactory, requestWrapper);
+const requestManager = new RequestManager(streamFactory, tokenizer);
+const warden = new Warden(requestManager, requestWrapper);
+
+module.exports = warden;
