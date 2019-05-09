0c6b1239960f72c049ed8c263659734374f3d8bf
Ahmet Can Güven <ahmetcanguven44@gmail.com>
2019-05-09 22:13:40 +0300

    Couchbase Plugin

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import {StreamFactory} from "./stream-factory";
 import {RequestManager} from "./request-manager";
 import {Warden} from "./warden";
 
+export {CouchbaseCache} from "./couchbase-cache";
+
 const cacheFactory = new CacheFactory();
 const tokenizer = new Tokenizer();
 const requestWrapper = new RequestWrapper();
@@ -12,4 +14,6 @@ const streamFactory = new StreamFactory(cacheFactory, requestWrapper);
 const requestManager = new RequestManager(streamFactory, tokenizer);
 const warden = new Warden(requestManager, requestWrapper, cacheFactory);
 
-export default warden;
+export {
+  warden
+};
